3d5f37efba0b9c76de5b08fa1eba4ebb8ff19875
aniwag2 <aniwaghray@gmail.com>
2024-12-02 03:15:52 -0500

    Fixing processes.c

--- src/processes.h
+++ src/processes.h
@@ -3,8 +3,13 @@
 
 #include <gtk/gtk.h>
 
+typedef struct {
+    GtkTreeStore *store;
+    GtkToggleButton *toggle_button;
+} ProcessData;
+
 GtkWidget* create_process_tab();
-void update_process_list(GtkTreeStore *store);
+gboolean update_process_list(gpointer data);
 void on_treeview_right_click(GtkWidget *treeview, GdkEventButton *event, gpointer data);
 void on_row_activated(GtkTreeView *treeview, GtkTreePath *path, GtkTreeViewColumn *column, gpointer data);
 void show_detailed_view(guint pid);
@@ -12,5 +17,7 @@ void get_process_details(guint pid, char *details, size_t size);
 void stop_process_action(GtkMenuItem *item, gpointer data);
 void continue_process_action(GtkMenuItem *item, gpointer data);
 void kill_process_action(GtkMenuItem *item, gpointer data);
+void list_memory_maps_action(GtkMenuItem *item, gpointer data);
+void list_open_files_action(GtkMenuItem *item, gpointer data);
 
 #endif // PROCESSES_H
